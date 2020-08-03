9639d8b1e26e50a71a73a66cd38d84f1a375347c
TimeToStop <evgrat123@mail.ru>
2020-08-03 16:02:57 +0300

    Initial commit

--- Assets/Scripts/MainCamera.cs
+++ Assets/Scripts/MainCamera.cs
@@ -7,17 +7,23 @@ public class MainCamera : MonoBehaviour
     [SerializeField]
     private GameObject _main_player;
 
-    private Vector3 position;
+    private Vector3 _position;
 
     private void Start()
     {
-        position = transform.position;
-        position.x = _main_player.transform.position.x;
+        if (_main_player != null)
+        {
+            _position = transform.position;
+            _position.x = _main_player.transform.position.x;
+        }
     }
 
     public void Update()
     {
-        position.x = _main_player.transform.position.x;
-        transform.position = Vector3.Lerp(transform.position, position, Time.deltaTime);
+        if (_main_player != null)
+        {
+            _position.x = _main_player.transform.position.x;
+            transform.position = Vector3.Lerp(transform.position, _position, Time.deltaTime);
+        }
     }
 }
